a2974af3316de0f43a8def019e5e99dae1d3c6ec
Shahriat Hossain <somel90@gmail.com>
2021-03-03 08:21:59 +0600

    Spring Boot: Spring Data JPA Real-Time Project

---
+++ 23-spring-data-jpa-cruddemo/src/main/java/com/mycode/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.mycode.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mycode.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+}
