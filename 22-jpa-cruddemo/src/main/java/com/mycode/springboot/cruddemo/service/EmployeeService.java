d9e0f433df6e645cea063ff1e5f41663709075e2
Shahriat Hossain <somel90@gmail.com>
2021-03-03 07:37:30 +0600

    Spring Boot: Build a REST CRUD API with JPA - Real-Time Project

---
+++ 22-jpa-cruddemo/src/main/java/com/mycode/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.mycode.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.mycode.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
