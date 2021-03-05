95d891c07dc6643f3a11cc8ee7eb894b4160ee17
Shahriat Hossain <somel90@gmail.com>
2021-03-05 11:55:22 +0600

    Spring Boot: Thymeleaf - CRUD Database Real-Time Project:Delete Employee

---
+++ 37-thymeleafdemo-employees-delete/src/main/java/com/mycode/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.mycode.springboot.thymeleafdemo.service;
+
+import java.util.List;
+
+import com.mycode.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
